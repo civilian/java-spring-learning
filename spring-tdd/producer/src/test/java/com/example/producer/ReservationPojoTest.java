418dafd0e08df1d555838b090dcab9aaab0e4ff6
Oscar Chamat <oscarchamat@useitweb.com>
2019-12-16 19:14:12 -0500

    Advancing in the tests.

--- spring-tdd/producer/src/test/java/com/example/producer/ReservationPojoTest.java
+++ spring-tdd/producer/src/test/java/com/example/producer/ReservationPojoTest.java
@@ -1,6 +1,10 @@
 package com.example.producer;
 
 import org.junit.Test;
+import org.assertj.core.api.Assertions;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 
 public class ReservationPojoTest {
@@ -9,5 +13,19 @@ public class ReservationPojoTest {
     public void create() throws Exception {
         Reservation re = new Reservation("1", "Jane");
         Assert.assertEquals(re.getName(), "Jane");
+        Assert.assertThat(re.getName(), Matchers.equalToIgnoringCase("jane"));
+        Assert.assertThat(re.getName(), new BaseMatcher(){
+            @Override
+            public boolean matches(Object item){
+                return Character.isUpperCase(((String) item).charAt(0) );
+            }
+
+            @Override
+            public void describeTo(Description description){
+                description.appendText("the name should be valid uppercase!");
+            }
+        });
+
+        Assertions.assertThat(re.getName()).isEqualToIgnoringCase("jane");
     }
 }
