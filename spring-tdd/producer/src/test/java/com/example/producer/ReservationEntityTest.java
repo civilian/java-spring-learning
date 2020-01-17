ea0757ef129ba2df92b7f4158b86c8034a64fc4a
Oscar Chamat <oscarchamat@useitweb.com>
2020-01-17 14:03:58 -0500

    Fixing an old test.

--- spring-tdd/producer/src/test/java/com/example/producer/ReservationEntityTest.java
+++ spring-tdd/producer/src/test/java/com/example/producer/ReservationEntityTest.java
@@ -2,17 +2,14 @@ package com.example.producer;
 
 import java.util.function.Predicate;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-@RunWith(SpringRunner.class)
 @DataMongoTest
 public class ReservationEntityTest {
 
@@ -25,11 +22,12 @@ public class ReservationEntityTest {
         Mono<Reservation> save = this.template.save(reservation);
         StepVerifier
             .create(save)
-            .expectNextMatches(new Predicate<Reservation>{
+            .expectNextMatches(new Predicate<Reservation>() {
                 @Override
                 public boolean test(Reservation reservation) {
-                    return false;
+                    return reservation.getName().equalsIgnoreCase("a") &&
+                        reservation.getId() != null;
                 }
-            })
+            });
     }
 }
--- spring-tdd/producer/src/test/java/com/example/producer/ReservationPojoTest.java
+++ spring-tdd/producer/src/test/java/com/example/producer/ReservationPojoTest.java
@@ -1,18 +1,17 @@
 package com.example.producer;
 
-import org.junit.Test;
-import org.assertj.core.api.Assertions;
 import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
 import org.hamcrest.Matchers;
-import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class ReservationPojoTest {
 
     @Test
     public void create() throws Exception {
         Reservation re = new Reservation("1", "Jane");
-        Assert.assertEquals(re.getName(), "Jane");
+        Assertions.assertEquals(re.getName(), "Jane");
         Assert.assertThat(re.getName(), Matchers.equalToIgnoringCase("jane"));
         
         Assert.assertThat(re.getName(), new BaseMatcher <String> (){
