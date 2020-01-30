4026e05d1daded9a9ff6b0fc0b6932d95687f5cd
Oscar Chamat <oscarchamat@useitweb.com>
2020-01-30 14:11:06 -0500

    Advancing with a test of the repository.

---
+++ spring-tdd/producer/src/test/java/com/example/producer/ReservationRepositoryTest.java
@@ -0,0 +1,13 @@
+package com.example.producer;
+
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.junit.jupiter.api.Test;
+
+@DataMongoTest
+public class ReservationRepositoryTest {
+
+    @Test
+    public void query() throws Exception {
+        
+    }
+}
